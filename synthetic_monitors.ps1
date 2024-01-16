50cac3d4c5c4ced2a658694b303fb6c185f61ccc
pnvnd <peter@datacrunch.ca>
2024-01-15 20:39:29 -0500

    Update: added code to mass update monitors

--- synthetic_monitors.ps1
+++ synthetic_monitors.ps1
@@ -10,6 +10,10 @@ Param(
     [String] $apiEndpoint
 )
 
+# $accountId = "1234567"
+# $apiKey = "NRAK-XXXXXXXXXXXXXXXXXXXXXXXXXXX"
+# $apiEndpoint = "https://api.newrelic.com/graphql"
+
 # Define New Relic API endpoint and headers
 # if region = US then $apiEndpoint = "https://api.newrelic.com/graphql"
 # if region = EU then $apiEndpoint = "https://api.eu.newrelic.com/graphql"
@@ -54,23 +58,135 @@ while ($cursor -ne "cursor:""""") {
 
   # Post request to GraphQL
   $REQUEST = Invoke-WebRequest -Uri $apiEndpoint `
-  -Method POST `
-  -Headers @{"Api-Key"=$apiKey} `
-  -ContentType "application/json" `
-  -Body $QUERY
+    -Method POST `
+    -Headers @{"Api-Key" = $apiKey } `
+    -ContentType "application/json" `
+    -Body $QUERY
 
   $RESPONSE = $REQUEST | Select-Object Content -ExpandProperty Content | ConvertFrom-Json
   
   # Loop through GraphQL query for each cursor
   if ($RESPONSE) {
-      $entities += $RESPONSE.data.actor.entitySearch.results.entities
-      $nextCursor = $RESPONSE.data.actor.entitySearch.results.nextCursor
-      $cursor = "cursor:""$nextCursor"""
-  } else {
-      throw "Query failed to run."
+    $entities += $RESPONSE.data.actor.entitySearch.results.entities
+    $nextCursor = $RESPONSE.data.actor.entitySearch.results.nextCursor
+    $cursor = "cursor:""$nextCursor"""
   }
-
+  else {
+    throw "Query failed to run."
+  }
 }
 
-# Change monitorType -like "*" to either "SIMPLE", "BROWSER", "CERT_CHECK", "SCRIPT_BROWSER", "SCRIPT_API" to filter
+# Export results to CSV file
 $entities | Where-Object { $_.monitorType -like "*" } | ConvertTo-CSV -NoTypeInformation | Add-Content -Path "synthetic_monitor_results.csv"
+
+# Change monitorType -like "*" to either "SIMPLE", "BROWSER", "CERT_CHECK", "SCRIPT_BROWSER", "SCRIPT_API" to filter
+$entityGuid = $entities | Where-Object { $_.monitorType -like "SIMPLE" } | Select-Object guid -ExpandProperty guid
+
+# Set Private Location GUID to be updated
+$privateLocationGuid = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
+# List to store failed guids
+$failedGuids = @()
+
+foreach ($guid in $entityGuid) {
+  $retryCount = 0
+
+  while ($true) {
+    # This is for SIMPLE (ping) monitors only, change monitor type as needed. 
+    # Note: SIMPLE does not use {guid: "$privateLocationGuid"} like the other monitor types
+    $UPDATE = @{"query" = @"
+mutation {
+  syntheticsUpdateSimpleMonitor(guid: "$guid", monitor: {locations: {private: "$privateLocationGuid"}}) {
+    monitor {
+      name
+      status
+      locations {
+        private
+      }
+    }
+    errors {
+      description
+    }
+  }
+}
+"@
+    } | ConvertTo-Json
+
+    # Post request to GraphQL, payload is JSON
+    $REQ = Invoke-WebRequest -Uri $apiEndpoint `
+      -Method POST `
+      -Headers @{"Api-Key" = $apiKey } `
+      -ContentType "application/json" `
+      -Body $UPDATE
+
+    if ($REQ.StatusCode -eq 200) {
+      $REP = $REQ | Select-Object Content -ExpandProperty Content | ConvertFrom-Json
+
+      if ($REP) {
+        $ERRORS = $REP.errors
+
+        if ($REP.data.syntheticsUpdateSimpleMonitor.errors) {
+          Write-Error "GraphQL Errors for $guid" 
+          Write-Error $($REP.data.syntheticsUpdateSimpleMonitor.errors)
+          Write-Output "Waiting for 60 seconds before retrying..."
+          Start-Sleep -Seconds 60
+          $retryCount++
+
+          if ($retryCount -ge 3) {
+            Write-Output "Max retry attempts reached for guid: $guid."
+            $failedGuids += $guid
+            break
+          }
+
+          continue  # Retry the current iteration
+        }
+
+        if (-not $ERRORS) {
+          Write-Output $REQ | Select-Object Content -ExpandProperty Content
+          break
+        }
+
+        else {
+          Write-Output "Error for guid $guid." 
+          Write-Output "Error Message: $($REP.errors.message)"
+          Write-Output "Waiting for 60 seconds before retrying..."
+          Start-Sleep -Seconds 60
+          $retryCount++
+
+          if ($retryCount -ge 3) {
+            Write-Output "Max retry attempts reached for guid: $guid."
+            $failedGuids += $guid
+            break
+          }
+
+          continue  # Retry the current iteration
+        }
+      }
+      else {
+        Write-Output "Unexpected response structure for guid: $guid. Retrying..."
+        Start-Sleep -Seconds 60
+        $retryCount++
+
+        if ($retryCount -ge 3) {
+          Write-Output "Max retry attempts reached for guid: $guid."
+          $failedGuids += $guid
+          break
+        }
+
+        continue  # Retry the current iteration
+      }
+    }
+    else {
+      Write-Output "Query failed to run with a $($REQ.StatusCode)."
+      Write-Output "Response: $REP)"
+      throw "Query failed to run with a $($REQ.StatusCode)."
+    }
+  }
+}
+
+# Output failed guids
+if ($failedGuids.Count -gt 0) {
+  Write-Output "Failed to update the following guids: $($failedGuids -join ', ')"
+}
+
+Write-Output "Completed running script."
