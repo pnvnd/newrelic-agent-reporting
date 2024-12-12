8cec553c8957c2cb9706fba567e81ffc3c93f00f
pnvnd <peter@datacrunch.ca>
2024-12-12 10:54:31 -0500

    add: cyberark secure credential script

--- cyberark_nr-secure_credentials.ps1
+++ cyberark_nr-secure_credentials.ps1
@@ -2,25 +2,25 @@ $ProgressPreference = "SilentlyContinue"
 
 $NewRelicGraphQLEndpoint = 'https://api.newrelic.com/graphql'
 $accountId = '1234567'
-$apiKey = 'NRAK-XXXX'
+$apiKey = 'NRAK-XXXXXXXXXXXXXXXXXXXXXXXXXXX'
 $secureCredentialName = 'MY_PASSWORD'
 
-$CyberarkEndpoint = 'https://my-cyberark-endpoint.com'
+$CyberarkEndpoint = 'https://raw.githubusercontent.com/pnvnd/newrelic-agent-reporting/refs/heads/main/cyberark.json'
 
 # Get CyberArk credentials
 $CyberarkRequest = Invoke-WebRequest -ContentType "application/json" -Uri $CyberarkEndpoint 
-
 $CyberarkResponse = $CyberarkRequest | Select-Object Content -ExpandProperty Content | ConvertFrom-Json
-
 $CyberarkPassword = $CyberarkResponse.Content
+# Write-Output $CyberarkPassword
 
 # Update secure credentials
 $MUTATION = @{"query" = @"
 mutation {
   syntheticsUpdateSecureCredential (
-    accountId: $accountId, 
-    key: $secureCredentialName, 
-    value: $CyberarkPassword) {
+    accountId: $accountId,
+    key: "$secureCredentialName",
+    value: "$CyberarkPassword",
+    description: "Description of MY_PASSWORD") {
     createdAt
     lastUpdate
     errors {
@@ -39,7 +39,9 @@ $updateSecureCredential = Invoke-WebRequest -Uri $NewRelicGraphQLEndpoint `
 
 $updateSecureCredentialResponse = $updateSecureCredential | Select-Object Content -ExpandProperty Content | ConvertFrom-Json
 
-if ($createExportResponse.errors) {
-    Write-Output "Error: $($updateSecureCredential.errors[0].message)"
+# Write-Output $updateSecureCredentialResponse
+
+if ($updateSecureCredentialResponse.errors) {
+    Write-Output "Error: $($updateSecureCredentialResponse.errors[0].message)"
     exit
 }
